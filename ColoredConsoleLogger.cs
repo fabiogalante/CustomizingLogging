b673bc20eb6473122606274c62a440358102d4f9
Fabio Galante Mans <fabiogalantemans@gmail.com>
2022-01-25 14:13:02 -0300

    Customizing Logging

---
+++ ColoredConsoleLogger.cs
@@ -0,0 +1,49 @@
+namespace LoggingSample
+{
+    public class ColoredConsoleLogger : ILogger
+    {
+
+        private static object _lock = new Object();
+
+        private readonly string _name;
+        private readonly ColoredConsoleLoggerConfiguration _config;
+
+        public ColoredConsoleLogger(string name, ColoredConsoleLoggerConfiguration config)
+        {
+            _name = name;
+            _config = config;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel == _config.LogLevel;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+            {
+                return;
+            }
+
+            lock (_lock)
+            {
+                if (_config.EventId == 0 || _config.EventId == eventId.Id)
+                {
+                    var color = Console.ForegroundColor;
+                    Console.ForegroundColor =
+                    _config.Color;
+                    Console.Write($"{logLevel} - ");
+                    Console.Write($"{eventId.Id} - {_name} - ");
+                    Console.Write($"{formatter(state, exception)}\n");
+                    Console.ForegroundColor = color;
+                }
+            }
+        }
+    }
+}
---
+++ CustomLogger.cs
@@ -0,0 +1,9 @@
+namespace LoggingSample
+{
+    public class ColoredConsoleLoggerConfiguration
+    {
+        public LogLevel LogLevel { get; set; } = LogLevel.Warning;
+        public int EventId { get; set; } = 0;
+        public ConsoleColor Color { get; set; } = ConsoleColor.Yellow;
+    }
+}
